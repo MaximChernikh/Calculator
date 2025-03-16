511fb8e23b9e2c2cdc729b379fdd9f06b22fea10
MaximChernikh <maximchernikh1329@gmail.com>
2025-03-16 11:54:55 +0300

    first commit

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Calculator
+    {
+        private int _firstNum;
+        private int _secondNum;
+
+        public Calculator(IOperatorStrategy operatorStrategy, int firstNum, int secondNum) 
+        {
+            OperatorStrategy = operatorStrategy;
+            _firstNum = firstNum;
+            _secondNum = secondNum;
+        }
+
+        public IOperatorStrategy OperatorStrategy { private get; set; }
+        
+        public void Operate()
+        {
+             OperatorStrategy.Operate(_firstNum, _secondNum);
+        }
+
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Calculator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите выражение: ");
+            string inputData = Console.ReadLine();
+            string[] splitedData = inputData.Split(' ');
+
+            Calculator calculator = new Calculator
+                (null, 
+                int.Parse(splitedData[0]), 
+                int.Parse(splitedData[2]));
+
+
+            switch (splitedData[1])
+            {
+                case "+":
+                    calculator.OperatorStrategy = new Plus();
+                    calculator.Operate();
+                    break;
+                case "-":
+                    calculator.OperatorStrategy = new Minus();
+                    calculator.Operate();
+                    break;
+                case "*":
+                    calculator.OperatorStrategy = new Multiply();
+                    calculator.Operate();
+                    break;
+                case "/":
+                    calculator.OperatorStrategy = new Divide();
+                    calculator.Operate();
+                    break;
+                default:
+                    Console.WriteLine("Неизвестная операция");
+                    break;
+            }
+
+            //string[] arrayOfInputData = inputData.Split(['+', '-', '/', '*']);
+            //for (int i = 0; i < arrayOfInputData.Length; i++)
+            //{
+            //    arrayOfInputData[i] = arrayOfInputData[i].Trim();
+            //}
+        }
+    }
+}
